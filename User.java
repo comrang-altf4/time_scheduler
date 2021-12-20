51441372fc7303e3066daa73a05a3e020068babc
chinezeboi-08 <71803948+chinezeboi-08@users.noreply.github.com>
2021-12-20 11:01:56 +0100

    Add files via upload

--- User.java
+++ User.java
@@ -1,16 +1,22 @@
 import org.mindrot.jbcrypt.BCrypt;
+import javax.activation.DataHandler;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
 
 class SQLConnection {
     private static Statement statement;
-    private static Connection connection;
+
     SQLConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(
+            Connection connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/Java","root","eKak5CxcBsr");
             statement = connection.createStatement();
         }
@@ -19,9 +25,44 @@ class SQLConnection {
         }
     }
 
+    public static void sendEmail(String receiver, String option) {
+        String sender = "howard.su.08012001@gmail.com";
+        String host = "smtp.gmail.com";
+
+        Properties property = System.getProperties();
+        property.put("mail.smtp.host", host);
+        property.put("mail.smtp.port", "465");
+        property.put("mail.smtp.ssl.enable", "true");
+        property.put("mail.smtp.auth", "true");
+
+        Authenticator authenticator = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(sender, "HowardSu08012001");
+            }
+        };
+
+        Session session = Session.getInstance(property, authenticator);
+        session.setDebug(true);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender));
+            message.addRecipients(Message.RecipientType.TO, receiver);
+
+            int verification = ThreadLocalRandom.current().nextInt(100000, 1000000);
+            message.setSubject("Verification code!!");
+            message.setText("Your code is: " + verification);
+
+            Transport.send(message);
+            System.out.println("Sent!!");
+        }
+        catch (Exception E) {
+            System.out.println(E);
+        }
+    }
+
     public static void addUserDB(User info) {
         try {
-            statement = connection.createStatement();
             ResultSet result = statement.executeQuery("SELECT USERNAME\n" +
                     "FROM ACCOUNTS\n" + "WHERE USERNAME = '" + info.username +
                     "' AND EMAIL = '" + info.email + "' ;");
@@ -39,6 +80,20 @@ class SQLConnection {
         }
     }
 
+    public static void loginUser(User info) {
+        try {
+            ResultSet result = statement.executeQuery("SELECT HASHPASS\n" +
+                    "FROM ACCOUNTS\n" + "WHERE USERNAME = '" + info.username + "';");
+            result.next();
+            if(BCrypt.checkpw(info.password, result.getString(1)))
+                System.out.println("Welcome");
+            else
+                System.out.println("Wrong username/password");
+        }
+        catch (Exception E) {
+            System.out.println(E);
+        }
+    }
 }
 
 class User {
@@ -53,9 +108,8 @@ class User {
 }
 
 class Main {
-    public static void main(String[] args) {
-        SQLConnection begin = new SQLConnection();
-        User user1 = new User("HowardSu1", "123456789", "howardsu@gmail.com");
-        begin.addUserDB(user1);
+    public static void main(String args[]) {
+        SQLConnection system = new SQLConnection();
+        system.sendEmail("howardsu08@gmail.com", "A");
     }
 }
