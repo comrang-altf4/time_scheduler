834638b6820d0a9317b27aa73aac2dfd150ac766
chinezeboi-08 <71803948+chinezeboi-08@users.noreply.github.com>
2021-12-22 16:45:16 +0100

    Add files via upload

--- User.java
+++ User.java
@@ -7,5 +7,22 @@ class User {
         this.password = password;
         this.email = email;
     }
-
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
