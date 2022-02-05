596fd29727d2d3e397d09332795d3ed0d08426f7
tanln123 <69809186+tanln123@users.noreply.github.com>
2022-02-05 03:09:42 +0100

    Add icon and title to the window

--- ProjectJava/src/main/java/project/Main.java
+++ ProjectJava/src/main/java/project/Main.java
@@ -6,11 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class Main extends Application {
     protected static Sess1on session = new Sess1on();
@@ -21,13 +22,20 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException, SQLException, ClassNotFoundException {
+    public void start(Stage stage) throws IOException {
         // Load login screen
         Parent parent = FXMLLoader.load(getClass().getResource("/login-view.fxml"));
         Group group = new Group(parent);
         stage.setScene(new Scene(group));
         stage.initStyle(StageStyle.DECORATED);
         stage.show();
+
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Kalendar");
+        stage.setResizable(false);
+
+
     }
 
     public static void main(String[] args) {
